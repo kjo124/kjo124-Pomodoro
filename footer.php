a4b7ad64831eda3b0a266ac9f9ab30a3beec32e5
kjo124 <kylejodin@gmail.com>
2017-11-09 11:40:01 -0700

    display pomodoro table

--- footer.php
+++ footer.php
@@ -6,4 +6,20 @@
 	echo date('Y-m-d g:i:s');
 ?>
 </div>
+<br>
+<br>
+<?php
+$dbh = new PDO("sqlite:./pomodoro.db");
+$query="SELECT * FROM pomodoros";
+$result =  $dbh -> query($query);
+
+while ($row = mysql_fetch_array($results)) {
+    echo '<tr>';
+    foreach($row as $field) {
+        echo '<td>' . htmlspecialchars($field) . '</td>';
+    }
+    echo '</tr>';
+}
+ ?>
+
 </html>
