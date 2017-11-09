ede4d108bd7ae1b9e0bc983b005f9bb4f07c26b5
kjo124 <kylejodin@gmail.com>
2017-11-09 08:04:21 -0700

    fix????

--- pomodoroCount.php
+++ pomodoroCount.php
@@ -2,7 +2,20 @@
   header ( 'Content-Type: text/json' );
   header ( "Access-Control-Allow-Origin: *" );
   $dbh = new PDO("sqlite:./pomodoro.db");
-  $sql = $dbh->query("SELECT COUNT(*) FROM pomodoros");
-  $count = $sql->fetchColumn();
+  $count = null;
+  $result =  $dbh -> query("SELECT COUNT(*) FROM pomodoros");
+
+  if(!empty($result)) {
+    $count  = $result->fetchColumn(0);
+  } else{
+    //assuming PDO
+    print_r($dbh->errorInfo());
+  }
+
+
+
+
+  // $sql = $dbh->query("SELECT COUNT(*) FROM pomodoros");
+  //$count = $sql->fetchColumn();
   echo json_encode ($count);
 ?>
