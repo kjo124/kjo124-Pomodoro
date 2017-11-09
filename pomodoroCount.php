9b738d115bf2061e39350914d7c834327007905f
kjo124 <kylejodin@gmail.com>
2017-11-09 07:50:09 -0700

    semicolon

--- pomodoroCount.php
+++ pomodoroCount.php
@@ -1,8 +1,8 @@
 <?php
   header ( 'Content-Type: text/json' );
-  header ( "Access-Control-Allow-Origin: *" )
+  header ( "Access-Control-Allow-Origin: *" );
   $dbh = new PDO("sqlite:./pomodoro.db");
 	$pomodoro_num = $dbh->query("SELECT count(*) FROM pomodoros");
   $a = $pomodoro_num->fetchColumn();
-  echo json_encode ( $a );
+  echo json_encode ($a);
 ?>
