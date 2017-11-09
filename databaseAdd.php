9d6c2b84370898d2910c7803a6d29b9287bad1e4
kjo124 <kylejodin@gmail.com>
2017-11-09 08:58:53 -0700

    add start time not end time

--- databaseAdd.php
+++ databaseAdd.php
@@ -1,6 +1,8 @@
 <?php
   $dbh = new PDO("sqlite:./pomodoro.db");
-  $date = date('Y-m-d g:i:s');
+  $newTime = strtotime('-25 minutes');
+  $date = date('Y-m-d H:i:s', $newTime);
+  echo $date;
   $sql = "insert into pomodoros values('$date');";
   $status = $dbh->exec($sql);
  ?>
