5f07e06003ce4b2865a59256fc42cedbb0e95f23
kjo124 <kylejodin@gmail.com>
2017-11-09 10:54:05 -0700

    testing

--- index.php
+++ index.php
@@ -220,6 +220,7 @@ function countdown( elementName, seconds ){
               assignment: document.getElementById('specASDA').value}),
           // if that succeded
           success: function(result, toGive){
+            console.log(toGive);
             console.log(result);
             // update the count displayed
             jQuery.post("pomodoroCount.php", {}, function(data) {
