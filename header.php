0f5b1c187b947f6a5cf2cdcf34f06994c702fecc
kjo124 <kylejodin@gmail.com>
2017-11-09 08:54:01 -0700

    last commit broke everything

--- header.php
+++ header.php
@@ -10,7 +10,6 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="timer.js"></script>
 	<link rel="stylesheet" href="styles.css">
 	<title>Pomodoro Timer</title>
 </head>
