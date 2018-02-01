5bb47fc98d44b91b2ee0862a54ff4e461a1506fa
kjo124 <kylejodin@gmail.com>
2018-02-01 16:30:59 -0700

    getting ready for the work on the records section

--- app/app.module.js
+++ app/app.module.js
@@ -5,3 +5,7 @@ angular.module('timerApp', [
   // ...which depends on the `timer` module
   'timer'
 ]);
+angular.module('recordsApp', [
+  // ...which depends on the `timer` module
+  'records'
+]);
---
+++ app/records/records.component.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Register `phoneList` component, along with its associated controller and template
+angular.
+  module('records').
+  component('records', {
+    templateUrl: 'records/records.template.html',
+    controller: function recordsController() {
+
+    }
+  });
