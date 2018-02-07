de1add5b122294eb2be88d5a63cd1c953c53baa9
Kyle Odin <kjo124@topeka.cs.colostate.edu>
2018-02-07 16:43:02 -0700

    need to manual check

---
+++ app/records/addToDatabase.py
@@ -0,0 +1,47 @@
+#! /usr/bin/python3 -Wd
+
+# a script for adding new pomodoros to the database, also updates the JSON file
+# for the site when a pomodoro is added
+# sample run: python3 addToDatabase.py 2018-01-18 12:25:13 Personal Personal test
+import sqlite3
+import json
+import sys
+
+# connect to database
+conn = sqlite3.connect('../pomodoro.db')
+debug = False;
+
+# used for producing the JSON file
+def json_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+# all the heavy lifting for adding to the database handled here
+def addToDatabase(startDate,startTime,thisClass,thisType,assignment):
+    if debug:
+        print("startDate: " + startDate + "\n" + "startTime: " + startTime +
+        "\n" + "thisClass: " + thisClass + "\n" + "type: " + thisType + "\n" +
+        "assignment: " + assignment)
+
+    # the entry to be added
+    entry = [(startDate, startTime, thisClass, thisType, assignment)]
+    # place it into the row
+    c.executemany("insert into pomodoros values(?, ?, ?, ?, ?)", entry )
+    # save the table
+    conn.commit()
+    # every time you add a row might as well regenerate the JSON while were connected
+    c.execute("select * from pomodoros")
+    results = c.fetchall()
+    with open('records.json', 'w') as outfile:
+        json.dump(results, outfile)
+
+conn.row_factory = json_factory
+c = conn.cursor()
+
+# example
+# addToDatabase("2018-01-18", "12:25:13", "test", "Personal", "Job")
+addToDatabase(str(sys.argv[1]), str(sys.argv[2]), str(sys.argv[3]), str(sys.argv[4]), str(sys.argv[5]))
+
+conn.close()
--- app/records/databaseAdd.php
+++ app/records/databaseAdd.php
@@ -1,23 +1,28 @@
 <?php
-echo "hello";
-  // $dbh = new PDO("sqlite:./pomodoro.db");
-  // $sub25Min = strtotime('-25 minutes');
-  // $startDate = date('Y-m-d', $sub25Min);
-  // echo $startDate;
-  // // start time
-  // $startTime = date('H:i:s', $sub25Min);
-  // echo $startTime;
-  //
-  // $class = $_POST['class'];
-  // echo $class;
-  // $type = $_POST['type'];
-  // echo $type;
-  // $assignment = $_POST['assignment'];
-  // echo $assignment;
-  //
+  $sub25Min = strtotime('-25 minutes');
+  $startDate = date('Y-m-d', $sub25Min);
+  echo $startDate;
+  // start time
+  $startTime = date('H:i:s', $sub25Min);
+  echo $startTime;
+  
+  $class = $_POST['classArg'];
+  echo $class;
+  $type = $_POST['type'];
+  echo $type;
+  $assignment = $_POST['assignment'];
+  echo $assignment;
+  
   // // CREATE TABLE pomodoros (startDate date, startTime time, class varchar(50), type varchar(250), assignment varchar(250));
   // // insert into pomodoros values("2017-1-1", "12:00:13", "CS356", "Quiz", "Quiz 1");
   //
   // $sql = "insert into pomodoros values('$startDate', '$startTime', '$class', '$type', '$assignment');";
   // $status = $dbh->exec($sql);
+  $command = 'python3 addToDatabase.py '.'$startDate '.'$startTime '.'$class '.'$type '.'$assignment';
+
+  $escaped_command = escapeshellcmd($command);
+ 
+  $output = shell_exec($command);
+
+  echo $output;
  ?>
