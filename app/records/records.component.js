bf3007f264346e3f39728da3406231c7a804065a
kjo124 <kylejodin@gmail.com>
2018-02-07 12:37:42 -0700

    working on records display, formatted table adding search next

--- app/records/records.component.js
+++ app/records/records.component.js
@@ -6,6 +6,26 @@ angular.
   component('records', {
     templateUrl: 'records/records.template.html',
     controller: function recordsController() {
-
+      this.records = [
+        {
+          startDate: '2018-01-18',
+          startTime: '12:25:13',
+          class: 'Personal',
+          type: 'Personal',
+          assignment: 'Job'
+        }, {
+          startDate: '2018-01-24',
+          startTime: '16:17:20',
+          class: 'CS430',
+          type: 'Homework',
+          assignment: 'A1'
+        }, {
+          startDate: '2018-01-28',
+          startTime: '12:51:28',
+          class: 'JTC413',
+          type: 'Homework',
+          assignment: 'Homework #1'
+        }
+      ];
     }
   });
