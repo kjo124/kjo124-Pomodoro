f3ef794baced4ceb04d2fba64deee46e15a1e6d4
Kyle Odin <kjo124@juneau.cs.colostate.edu>
2018-02-08 09:50:25 -0700

    Looks like everything works on the timer now

--- app/timer/timer.component.js
+++ app/timer/timer.component.js
@@ -7,8 +7,13 @@ angular.
     controller: function timerController($scope,$timeout,$http) {
         $scope.counter = 5;
         $scope.stopped = true;
-        $scope.buttonText='Start';
+        $scope.pomodoroButtonText='Start Pomodoro';
+	$scope.shortButtonText='5 Minute Break';
+	$scope.longButtonText='10 Minute Break';
         $scope.displayedTimer = "00:00";
+	$scope.showPomodoro = true;
+	$scope.showShort = true;
+	$scope.showLong = true;
         $scope.humanReadableTimer = function() {
           function z(n) {
             return (n<10? '0' : '') + n;
@@ -24,9 +29,8 @@ angular.
             if ($scope.counter > 0) {
               mytimeout = $timeout($scope.onTimeout,1000);
             } else {
-              alert("Time is up!");
-              $scope.buttonText='Restart';
-	           var xsrf = $.param({
+	      if ($scope.pomodoroButtonText=='Cancel Pomodoro') {
+		var xsrf = $.param({
 		                  classArg:document.getElementById('classSelect').value,
 		                  type:document.getElementById('assSelect').value,
 		                  assignment:document.getElementById('specSelect').value});
@@ -42,23 +46,73 @@ angular.
                 console.log("error");
                 $scope.error = response.statusText;
               });
+	      }
+              alert("Time is up!");
+              $scope.pomodoroButtonText='Start Pomodoro';
+	      $scope.shortButtonText='5 Minute Break';
+	      $scope.longButtonText='10 Minute Break';
+	      $scope.showPomodoro = true;
+	      $scope.showShort = true;
+	      $scope.showLong = true;
             }
           }
         }
         var mytimeout;
-        $scope.takeAction = function() {
-          if(!$scope.stopped && $scope.buttonText=='Stop'){
+        $scope.pomodoroClick = function() {
+          if(!$scope.stopped && $scope.pomodoroButtonText=='Cancel Pomodoro'){
             $timeout.cancel(mytimeout);
-            $scope.buttonText='Start';
-          } else if (!$scope.stopped && $scope.buttonText=='Restart') {
+            $scope.pomodoroButtonText='Start Pomodoro';
+	    $scope.showShort = true;
+	    $scope.showLong = true;
+            $scope.displayedTimer = "00:00";
+          } else if ($scope.stopped && $scope.pomodoroButtonText=='Start Pomodoro'){
             mytimeout = $timeout($scope.onTimeout,1000);
-            $scope.counter = 10;
-            $scope.buttonText='Stop';
-            $scope.stopped =!$scope.stopped;
+	    $scope.counter = 1500;
+            $scope.pomodoroButtonText='Cancel Pomodoro';
+	    $scope.showShort = false;
+	    $scope.showLong = false;
           } else {
+	    mytimeout = $timeout($scope.onTimeout,1000);
+            $scope.pomodoroButtonText='Cancel Pomodoro';
+	  }
+          $scope.stopped=!$scope.stopped;
+        }
+	$scope.shortClick = function() {
+          if(!$scope.stopped && $scope.shortButtonText=='Cancel 5 Minute Break'){
+            $timeout.cancel(mytimeout);
+            $scope.shortButtonText='5 Minute Break';
+	    $scope.showPomodoro = true;
+	    $scope.showLong = true;
+	    $scope.displayedTimer = "00:00";
+          } else if ($scope.stopped && $scope.shortButtonText=='5 Minute Break'){
             mytimeout = $timeout($scope.onTimeout,1000);
-            $scope.buttonText='Stop';
-          }
+	    $scope.counter = 300;
+            $scope.shortButtonText='Cancel 5 Minute Break';
+	    $scope.showPomodoro = false;
+	    $scope.showLong = false;
+          } else {
+	    mytimeout = $timeout($scope.onTimeout,1000);
+            $scope.shortButtonText='Cancel 5 Minute Break';
+	  }
+          $scope.stopped=!$scope.stopped;
+        }
+	$scope.longClick = function() {
+          if(!$scope.stopped && $scope.longButtonText=='Cancel 10 Minute Break'){
+            $timeout.cancel(mytimeout);
+            $scope.longButtonText='10 Minute Break';
+	    $scope.showPomodoro = true;
+	    $scope.showShort = true;
+	    $scope.displayedTimer = "00:00";
+          } else if ($scope.stopped && $scope.longButtonText=='10 Minute Break'){
+            mytimeout = $timeout($scope.onTimeout,1000);
+	    $scope.counter = 600;
+            $scope.longButtonText='Cancel 10 Minute Break';
+	    $scope.showPomodoro = false;
+	    $scope.showShort = false;
+          } else {
+	    mytimeout = $timeout($scope.onTimeout,1000);
+            $scope.longButtonText='Cancel 10 Minute Break';
+	  }
           $scope.stopped=!$scope.stopped;
         }
     }
