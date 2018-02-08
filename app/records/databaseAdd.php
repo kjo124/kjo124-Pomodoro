2c6f169e63baa8a9ee01cd6167afe9dd94423636
Kyle Odin <kjo124@juneau.cs.colostate.edu>
2018-02-08 08:58:03 -0700

    fixed what went wrong during last 2 commits and fixed mobile

--- app/records/databaseAdd.php
+++ app/records/databaseAdd.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
   $sub25Min = strtotime('-25 minutes');
   $startDate = date('Y-m-d', $sub25Min);
   echo $startDate;
@@ -19,27 +18,4 @@
   $escaped_command = escapeshellcmd($command);
 
   system($escaped_command);
-=======
-echo "hello";
-  // $dbh = new PDO("sqlite:./pomodoro.db");
-  // $sub25Min = strtotime('-25 minutes');
-  // $startDate = date('Y-m-d', $sub25Min);
-  // echo $startDate;
-  // // start time
-  // $startTime = date('H:i:s', $sub25Min);
-  // echo $startTime;
-  //
-  // $class = $_POST['class'];
-  // echo $class;
-  // $type = $_POST['type'];
-  // echo $type;
-  // $assignment = $_POST['assignment'];
-  // echo $assignment;
-  //
-  // // CREATE TABLE pomodoros (startDate date, startTime time, class varchar(50), type varchar(250), assignment varchar(250));
-  // // insert into pomodoros values("2017-1-1", "12:00:13", "CS356", "Quiz", "Quiz 1");
-  //
-  // $sql = "insert into pomodoros values('$startDate', '$startTime', '$class', '$type', '$assignment');";
-  // $status = $dbh->exec($sql);
->>>>>>> parent of de1add5... need to manual check
  ?>
